f296ce57e4602930fcdf5342559d0cfc776316ef
amar <amarshil1234@gmail.com>
2026-09-17 16:49:10 +0100

    test 1

--- Level 1.py
+++ Level 1.py
@@ -1,1 +1,35 @@
-print("Welcome to the Level 1 game")
+#print("Welcome to the Level 1 game")
+
+# 3 life
+#Mistake
+#life = 3
+#while life > 0:
+#    question = input("did you win or lose: ")
+#    if question == "lose":
+#        life -= 1
+#        print("You lose")
+#    elif question == "win":
+#        print("You win")
+ #       break
+  #  else:
+   #     print("You lost")
+
+#correction
+
+#life = 3
+
+#while life > 0:
+ #   question = input("Did you win or lose? ")
+
+  #  if question == "lose":
+   #     life -= 1
+    #    print("You lose")
+     #   print("Lives remaining:", life)
+
+    #elif question == "win":
+     #   print("You win")
+
+    #else:
+     #   print("Invalid input. Please enter win or lose.")
+
+#print("Game Over")
