908ad4ec4f8f62e330c988c7fa5ffd622db8ccea
amar <amarshil1234@gmail.com>
2026-09-22 08:20:51 +0100

    Health analyser

--- Level 1.py
+++ Level 1.py
@@ -278,9 +278,15 @@ from importlib.metadata import pass_none
 #        print("Strongest enemy health: ", en)
 
 #Correct
-enemies = [25, 70, 45, 90, 60, 30]
-strongest = 0
-for health in enemies:
-    if health > strongest:
-        strongest = health
-print("Strongest enemy health:", strongest)
+#enemies = [25, 70, 45, 90, 60, 30]
+#strongest = 0
+#for health in enemies:
+#    if health > strongest:
+#        strongest = health
+#print("Strongest enemy health:", strongest)
+
+health = 100
+
+while health > 0:
+    question = int(input("How much damage did the enemy deal? : "))
+    if
